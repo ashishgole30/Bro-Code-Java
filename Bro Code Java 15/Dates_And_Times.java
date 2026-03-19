4102beaa6a7814a92bd9bda9b2101790266f924c
ashishgole30 <ashish.gole3010@gmail.com>
2026-03-19 23:59:26 +0530

    m

--- Bro Code Java 15/Dates_And_Times.java
+++ Bro Code Java 15/Dates_And_Times.java
@@ -1,8 +1,16 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Dates_And_Times {
     public static void main(String[] args) {
 
         // How to work with Dates & Times using java
         // (Localdate , localtime, localdatetime, UTC timestamp)
 
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
     }
 }
